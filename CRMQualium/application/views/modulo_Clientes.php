e4546416069e7e88cb5c959d13515d4b7563ad0f
Rodrigo Viana <rodviana92@gmail.com>
2014-04-02 11:28:32 -0600

    otro commit clientes

--- CRMQualium/application/views/modulo_Clientes.php
+++ CRMQualium/application/views/modulo_Clientes.php
@@ -4,7 +4,7 @@
 		   <nav>
 				<ul id="menu_modulo" class="nav nav-pills">
 					<li>
-						<a href="modulo_cliente_nuevo">
+						<a href="">
 						<div class="icono_menu_modulo">
 						<span class="icon-contact"></span></div>
 						Prospectos
@@ -19,21 +19,19 @@
 				        </a>
 					</li> 
          			
-         			<li class="dropdown">
-					    	<a href="modulo_cliente_nuevo" class="dropdown-toggle" data-toggle="dropdown">
+         			<li id="tamaño" class="dropdown">
+					    	<a href= class="dropdown-toggle" data-toggle="dropdown">
 								<div class="icono_menu_modulo">
 								<span class="icon-uniF476"></span>
 								</div>
 								Nuevo
 							</a>
 		   		    	<ul id="prueba" class="dropdown-menu" role="menu">
-    						<li><a href="#">Cliente</a></li>
+    						<li><a href="modulo_cliente_nuevo">Cliente</a></li>
     						<li><a href="#">Prospectos</a></li>
   				    	</ul>
-			        </li>	
-
-			        
-			     </ul>
+			        </li>
+			   </ul>
 		  </nav>
 		  
 	</section>
