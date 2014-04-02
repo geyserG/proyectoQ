bc0fdaa76f18f082264f568fe91492cc4afc3dd6
maykultman <dolarmkm@gmail.com>
2014-04-01 22:38:33 -0600

    cliente y cotizaciones
    beto

--- CRMQualium/application/views/modulo_Clientes.php
+++ CRMQualium/application/views/modulo_Clientes.php
@@ -4,14 +4,14 @@
 		   <nav>
 				<ul id="menu_modulo" class="nav nav-pills">
 					<li>
-						<a href="#">
+						<a href="modulo_cliente_nuevo">
 						<div class="icono_menu_modulo">
 						<span class="icon-contact"></span></div>
 						Prospectos
 			            </a>
 					</li>
 		  			<li>
-						<a href="#">
+						<a href="">
 						<div class="icono_menu_modulo">
 						<span class="icon-phpbb"></span>
 					    </div>
@@ -20,7 +20,7 @@
 					</li> 
          			
          			<li class="dropdown">
-					    	<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+					    	<a href="modulo_cliente_nuevo" class="dropdown-toggle" data-toggle="dropdown">
 								<div class="icono_menu_modulo">
 								<span class="icon-uniF476"></span>
 								</div>
--- CRMQualium/application/views/modulo_cotizaciones.php
+++ CRMQualium/application/views/modulo_cotizaciones.php
@@ -1,15 +1,10 @@
-<!-- <div id="contenedor"> -->
 	<div id="contenido_modulo_cotizacion">
 		<section id="cabecera_modulo">
 			<h1 id="titulo_del_modulo"><label>Cotizaciones</label></h1>
 			<nav>
 				<ul id="menu_modulo" class="nav nav-pills">
 					<li>
-<<<<<<< HEAD
-						<a href="modulo_cotizaciones">
-=======
 						<a href="modulo_cotizaciones_consulta">
->>>>>>> 28af385c7042a65b2d8e4675058722cf16e64473
 							<div class="icono_menu_modulo">
 								<span class="icon-rawaccesslogs"></span>
 							</div>
